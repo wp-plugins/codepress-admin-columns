318818b84443d93a49631843114d73aa49f50e0f
codepress <codepress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-13 22:25:08 +0000

    1.4.9
    git-svn-id: https://plugins.svn.wordpress.org/codepress-admin-columns/trunk@638877 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/third_party.php
+++ classes/third_party.php
@@ -13,11 +13,12 @@ function pre_load_wordpress_seo_class_metabox()
 {
 	global $pagenow;
 
-	if ( 
-		isset( $_REQUEST['page'] ) && 
-		'codepress-admin-columns' == $_REQUEST['page'] && 
-		'options-general.php' == $pagenow && 
-		defined('WPSEO_PATH') && file_exists(WPSEO_PATH.'admin/class-metabox.php')
+	if (
+		isset($_REQUEST['page']) &&
+		'codepress-admin-columns' == $_REQUEST['page'] &&
+		'options-general.php' == $pagenow &&
+		defined('WPSEO_PATH') &&
+		file_exists(WPSEO_PATH.'admin/class-metabox.php')
 		) {
 		require_once WPSEO_PATH.'admin/class-metabox.php';
 	}
@@ -27,14 +28,14 @@ add_action( 'plugins_loaded', 'pre_load_wordpress_seo_class_metabox', 0 );
 /**
  * Fix which remove the Advanced Custom Fields Type (acf) from the admin columns settings page
  *
- * @since     1.5
+ * @since 1.5
  */
 function remove_acf_from_cpac_post_types( $post_types )
 {
-	if ( class_exists('Acf') ) {		
+	if ( class_exists('Acf') ) {
 		unset( $post_types['acf'] );
 	}
-	
+
 	return $post_types;
 }
 add_filter( 'cpac-get-post-types', 'remove_acf_from_cpac_post_types' );
@@ -42,16 +43,29 @@ add_filter( 'cpac-get-post-types', 'remove_acf_from_cpac_post_types' );
 /**
  * Fix which removes bbPress Posttypes ( forum, reply and topic ) from the admin columns settings page
  *
- * @since     1.5
+ * @since 1.5
  */
 function remove_bbpress_from_cpac_post_types( $post_types )
 {
-	if ( class_exists('bbPress') ) {		
+	if ( class_exists('bbPress') ) {
 		unset( $post_types['topic'] );
 		unset( $post_types['reply'] );
 		unset( $post_types['forum'] );
 	}
-	
+
 	return $post_types;
 }
 add_filter( 'cpac-get-post-types', 'remove_bbpress_from_cpac_post_types' );
+
+/**
+ * Add support for All in SEO columns
+ *
+ * @since 1.5
+ */
+function cpac_load_aioseop_addmycolumns()
+{
+	if ( function_exists('aioseop_addmycolumns') ) {
+		aioseop_addmycolumns();
+	}
+}
+add_action( 'cpac-get-default-columns-posts', 'cpac_load_aioseop_addmycolumns' );
